97b8415d83034c3c3dea4960f856e5d802ed3d31
Ansh439 <gupta.ansh3150@gmail.com>
2024-07-11 15:54:43 +0530

    installed Tailwind Css and initialized server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome at port")
+})
+
+app.listen('8080', () => {
+    console.log("server starts running at port 8080");
+})
